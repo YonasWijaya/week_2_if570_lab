5bc825e68797e610c4e0d2c07a05f43ad6a86910
YonasWijaya <yonas.wijaya@student.umn.ac.id>
2023-02-22 21:01:25 +0700

    Getting started with filters

--- Hello_Kotlin/src/main/kotlin/Hello.kt
+++ Hello_Kotlin/src/main/kotlin/Hello.kt
@@ -1,46 +1,40 @@
-import java.util.*
+fun main() {
+    val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 
-fun isTooHot(temperature: Int) = temperature > 30
+    // eager, creates a new list
+//    val eager = decorations.filter { it [0] == 'p' }
+//    println("eager: $eager")
+//
+//    // lazy, will wait until asked to evaluate
+//    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+//    println("filtered: $filtered")
+//
+//    // force evaluation of the lazy list
+//    val newList = filtered.toList()
+//    println("new list: $newList")
 
-fun isDirty(dirty: Int) = dirty > 30
+//    val lazyMap = decorations.asSequence().map {
+//        println("access: $it")
+//        it
+//    }
+//    println("lazy: $lazyMap")
+//    println("-----")
+//    println("first: ${lazyMap.first()}")
+//    println("-----")
+//    println("all: ${lazyMap.toList()}")
 
-fun isSunday(day: String) = day == "Sunday"
+//    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+//        println("access: $it")
+//        it
+//    }
+//    println("-----")
+//    println("filtered: ${lazyMap2.toList()}")
 
-fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
-    return when {
-        isTooHot(temperature) -> true
-        isDirty(dirty) -> true
-        isSunday(day) -> true
-        else -> false
-    }
-}
+    val mysports = listOf("basketball", "fishing", "running")
+    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+    val mylist = listOf(mysports, myplayers, mycities) // list of lists
+    println("-----")
+    println("Flat: ${mylist.flatten()}")
 
-fun randomDay() : String {
-    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
-        "Friday", "Saturday", "Sunday")
-    return week[Random().nextInt(week.size)]
-}
-
-
-fun fishFood (day : String) : String {
-    return when (day) {
-        "Monday" -> "flakes"
-        "Wednesday" -> "redworms"
-        "Thursday" -> "granules"
-        "Friday" -> "mosquitoes"
-        "Sunday" -> "plankton"
-        else -> "nothing"
-    }
-}
-
-fun feedTheFish() {
-    val day = randomDay()
-    val food = fishFood(day)
-    println ("Today is $day and the fish eat $food")
-    println("Change water: ${shouldChangeWater(day)}")
-}
-
-
-fun main(args: Array<String>){
-    feedTheFish()
 }
