195dbb2327f3ff5f28221fcc6f096001431f2dab
YonasWijaya <yonas.wijaya@student.umn.ac.id>
2023-02-22 20:55:35 +0700

    Exploring default values and compact functions

--- Hello_Kotlin/src/main/kotlin/Hello.kt
+++ Hello_Kotlin/src/main/kotlin/Hello.kt
@@ -1,11 +1,28 @@
 import java.util.*
 
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
+}
+
 fun randomDay() : String {
-    val week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
     return week[Random().nextInt(week.size)]
 }
 
-fun fishFood (day :String) : String {
+
+fun fishFood (day : String) : String {
     return when (day) {
         "Monday" -> "flakes"
         "Wednesday" -> "redworms"
@@ -16,11 +33,13 @@ fun fishFood (day :String) : String {
     }
 }
 
-fun feedTheFish(){
+fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
-    println("Today is $day and the fish eat $food")
+    println ("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
 }
+
 
 fun main(args: Array<String>){
     feedTheFish()
